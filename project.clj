8a64e415ca2c3c3b4413c0dccd094c17b460eadd
Jeongbong Seo <jngbng@gmail.com>
2022-01-15 19:13:32 +0900

    change project name to deploy it

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject komcrad/repl-reload "0.1.2"
+(defproject org.clojars.bgpworks/repl-reload "0.1.3-SNAPSHOT"
   :description "Reloads the repl on file change"
-  :url "https://github.com/komcrad/repl-reload"
+  :url "https://github.com/bgpworks/repl-reload"
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3" :scope "provided"]
