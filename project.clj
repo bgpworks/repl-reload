14ff1e8a834fe77c21969573ce8e5cc22b58daa8
Jeongbong Seo <jngbng@gmail.com>
2022-01-15 19:27:43 +0900

    Version 0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.bgpworks/repl-reload "0.1.3-SNAPSHOT"
+(defproject org.clojars.bgpworks/repl-reload "0.1.3"
   :description "Reloads the repl on file change"
   :url "https://github.com/bgpworks/repl-reload"
   :license {:name "EPL-2.0"
