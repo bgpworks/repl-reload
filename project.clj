b8f88464a82cadf065e5292c066d675bd01348a6
Jeongbong Seo <jngbng@gmail.com>
2022-01-15 19:07:14 +0900

    dependencies version up

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/komcrad/repl-reload"
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/tools.namespace "1.0.0"]
+  :dependencies [[org.clojure/clojure "1.10.3" :scope "provided"]
+                 [org.clojure/tools.namespace "1.2.0"]
                  [ns-tracker "0.4.0"]]
   :repl-options {:init-ns repl-reload.core})
